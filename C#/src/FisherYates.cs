17a63cbddd9cfc5da738392f7f9734833966a8ea
Jonathan Moore <jdm7dv@gmail.com>
2016-12-16 07:10:59 -0500

    addded Fisher Yates

--- C#/src/FisherYates.cs
+++ C#/src/FisherYates.cs
@@ -1,17 +1,3 @@
-public void FisherYates(int[] ary)
-{
-    Random rnd = new Random();
-
-    for (int i = ary.Length - 1; i > 0; i--)
-    {
-        int r = rnd.Next(0, i);
-
-        int tmp = ary[i];
-        ary[i] = ary[r];
-        ary[r] = tmp;
-    }
-}
-
 using System;
 
 class Program
@@ -28,19 +14,33 @@ class Program
     /// <param name="array">Array to shuffle.</param>
     public static void Shuffle<T>(T[] array)
     {
-	var random = _random;
-	for (int i = array.Length; i > 1; i--)
-	{
-	    // Pick random element to swap.
-	    int j = random.Next(i); // 0 <= j <= i-1
-	    // Swap.
-	    T tmp = array[j];
-	    array[j] = array[i - 1];
-	    array[i - 1] = tmp;
-	}
+        var random = _random;
+        for (int i = array.Length; i > 1; i--)
+        {
+            // Pick random element to swap.
+            int j = random.Next(i); // 0 <= j <= i-1
+                                    // Swap.
+            T tmp = array[j];
+            array[j] = array[i - 1];
+            array[i - 1] = tmp;
+        }
     }
 
-    static void Main()
+    public void FisherYates(int[] ary)
+    {
+        Random rnd = new Random();
+
+        for (int i = ary.Length - 1; i > 0; i--)
+        {
+            int r = rnd.Next(0, i);
+
+            int tmp = ary[i];
+            ary[i] = ary[r];
+            ary[r] = tmp;
+        }
+    }
+}
+   /*static void Main()
     {
 	{
 	    int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
@@ -60,3 +60,4 @@ class Program
 	}
     }
 }
+*/
